d3a0b5d4a4d10e770f48a37a7e9c0e2ec2524dde
i.kozlov <kozlov.titan2010@yandex.ru>
2021-05-18 13:58:59 +0300

    Worflow. IvanKozlov. Обощение пагинации

--- __fast_api_app/models/general/system_routes/nsi_api/routes.py
+++ __fast_api_app/models/general/system_routes/nsi_api/routes.py
@@ -2,7 +2,8 @@
 Универсальное API для работы с моделями
 """
 
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
+from fastapi_pagination import Params, paginate
 
 from MODS.standart_namespace.routes import standardize_response
 from .tools.create_model import create_some
@@ -43,3 +44,19 @@ async def orm_post_model_list(client_key: str, dict_name: str, body=Body(...)):
     )
 
     return res
+
+
+@router.get("/page/{client_key}/{dict_name}")
+@standardize_response
+async def orm_get_model_list_page(client_key: str, dict_name: str, params: Params = Depends()):
+    """
+    Универсальное получение списка с пагинацией
+    """
+
+    res = await get_some(
+        client_key=client_key,
+        dict_name=dict_name,
+    )
+    page = paginate(res, params).json()
+    return page
+
