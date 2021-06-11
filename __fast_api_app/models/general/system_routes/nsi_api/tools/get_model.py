2d5fc566e5260d173e9b4f50c47b3c62c2605f3b
Иван Козлов <kozlov.titan2010@gmail.com>
2021-06-11 15:07:58 +0300

    Вернул случайно затёртый апдейт состояния

--- __fast_api_app/models/general/system_routes/nsi_api/tools/get_model.py
+++ __fast_api_app/models/general/system_routes/nsi_api/tools/get_model.py
@@ -6,6 +6,7 @@ from fastapi.encoders import jsonable_encoder
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 from MODS.storage_atlant_driver.pack_core.main import get_orm_class
+from MODS.rest_core.pack_core.system_models.system_models import tortoise_state
 
 
 class ORMProcessingError(Exception):
@@ -17,7 +18,9 @@ async def get_some(client_key, dict_name, **kwargs):
     Получить список моделей, одну модель с фильтрацией.
     Пока что есть только получение списка без фильтра
     """
-
+    state = await tortoise_state.state_check()
+    if not state:
+        await tortoise_state.state_activate()
     class_model = get_orm_class(client_key=client_key, dict_name=dict_name)
     if not class_model:
         raise ORMProcessingError('Model not found!')
